a24cfc3cbc0f5e2185df2a9cf2c87c65027a9134
uggin-mai <100463079+uggin-mai@users.noreply.github.com>
2022-03-26 11:44:14 +0300

    Add files via upload

---
+++ 21.sh
@@ -0,0 +1,28 @@
+﻿#!/usr/bin/env bash
+function CaesarCipher {
+    cat $file | tr "[A-Za-z]" "[D-ZA-Cd-za-c]" > $file
+    #printf "ok"
+}
+shopt -s globstar
+
+#read input
+#echo $1
+if [ -z $1 ]
+then
+    read -p "Введите каталог для кодирования:" dir
+elif [ $1 = "?" ]
+then
+    printf "CaesarCipher, использование - ./CS.sh [каталог для кодирования]\n"
+    exit 0
+else
+    dir=$1
+fi
+#read -p "Введите каталог для кодирования:" dir
+
+for file in ./$dir/**/* #input
+do
+    if [ -f "$file" ];
+    then
+        CaesarCipher "$file"
+    fi
+done
